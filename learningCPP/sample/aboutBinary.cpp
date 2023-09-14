186db1e97b36a6e7a7613d8dd1e599b15ae099a5
aDl2apple <liushiqi0812@gmail.com>
2023-09-14 09:27:38 +0800

    casnge dl

--- learningCPP/sample/aboutBinary.cpp
+++ learningCPP/sample/aboutBinary.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main(){
-    int d = 10;//十进制
-    int o = 010;//八进制 0-
-    int h = 0x10;//十六进制 0x-
-    std::cout << d << o << h << endl;
+    int d = 10;// 十进制
+    int o = 010;// 八进制 0-
+    int h = 0x10;// 十六进制 0x-
+    std::cout << d << o << h << std::endl;// 10 8 16
     return 0;
 }
---
+++ learningEditer/noteMarkdown.md
@@ -0,0 +1,5 @@
+# 笔记：Markdown 语法
+
+### 注意
+
+- **GFM（GitHub Flavored Markdown） 并不支持 DL（Description List）**
